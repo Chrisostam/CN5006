033bdd7ea9be2c0782713115b3dfd716172c79e2
Chrisostam <gopalicrisostam@gmail.com>
2024-12-19 20:44:13 +0000

    Update Delete_Book.js

--- week_10-11/frontend(w11)/components/Delete_Book.js
+++ week_10-11/frontend(w11)/components/Delete_Book.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from "react-router-dom";
-
 import DisplayData from './DisplayData';
+
 export default function DeleteBook(props) {
     const [state, setState] = useState([]);
     let params = useParams();
